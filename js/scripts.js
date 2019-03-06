4658fa5f7689285899f335f737f5e03afe9df6c2
fuaad001 <husseinfuaad@hotmail.com>
2019-03-06 20:41:05 +0300

    dealing with styles'

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//User Interface logic
 $(document).ready(function(){
   $(".one").click(function(){
     $(".nav-link").removeClass("selected");
@@ -59,4 +60,60 @@ $(document).ready(function(){
     $(".col-md-10").fadeOut();
     $(".others").fadeIn();
   });
+  $(".login").click(function(){
+    $(".login-block").show();
+  })
 });
+
+var elements = $(".column");
+var i;
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";
+    elements[i].style.flex = "100%";
+  }
+}
+
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";
+    elements[i].style.flex = "50%";
+  }
+}
+
+function three() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "33%";
+    elements[i].style.flex = "33%";
+  }
+}
+
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";
+    elements[i].style.flex = "25%";
+  }
+}
+
+function same() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";
+    elements[i].style.flex = "25%";
+    $(document).ready(function(){
+      $(".height").addClass("equal");
+    });
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+//Business logic
